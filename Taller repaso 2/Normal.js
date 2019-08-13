dd0bc453bb4c962ab0cc95f5cb80a13fc12b9d6d
Esteban V <53828773+stban2703@users.noreply.github.com>
2019-08-13 08:26:29 -0500

    Delete Normal.js

--- Taller repaso 2/Normal.js
+++
@@ -1,5 +0,0 @@
-class Normal extends Enemigo{
-    constructor(posX, posY, ancho, alto, r, g, b, resistencia, velocidad){
-    super(posX, posY, ancho, alto, r, g, b, resistencia, velocidad);
-    }
-}
